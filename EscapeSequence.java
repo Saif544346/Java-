4ad33529b04ff108d88e9963a065e80e8464c4d6
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2024-02-10 11:03:45 +0600

    Add files via upload

---
+++ EscapeSequence.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.ooplab;
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("Ibadullah saif");
+        System.out.println("Ibadullah saif \t \"Green University Of Bangladesh\"");
+        System.out.print("Ibadullah Saif\nBsc in Cse\nGreen University");
+    }
+    
+}
